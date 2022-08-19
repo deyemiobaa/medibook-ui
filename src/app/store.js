2390ed7be3d536fbab5534959ec632ec4614f369
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-19 11:23:07 +0100

    remove unused redux files

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../redux/counter/counterSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 });
 
-export default store
+export default store;

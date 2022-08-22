e9158fad0a529ccc77f6e5021f603a2bb8bb50ca
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-22 11:29:42 +0200

    Update style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import Navigation from '../Navigation/Navigation';
 const Home = () => (
   <div className="flex">
     <Navigation />
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font overflow-scroll">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <h1 className="text-2xl title-font mb-4 text-gray-900 font-black">
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,14 +4,14 @@ import logo from "../../assests/logo.png"
 
 export default function Navigation() {
   return (
-    <div className="navigation w-48 bg-gray-300  max-h-full flex flex-col max-h-full pl-[15px]">
+    <div className="navigation w-[25%] bg-gray-300  max-h-full flex flex-col max-h-full ">
       <div className="logo w-[70%] self-center">
         <img src={logo} className="logo rounded-full" alt="logo" />
       </div>
-      <div className="navLinks flex flex-col mt-44 ">
+      <div className="navLinks flex flex-col mt-44 pl-[15px]">
         <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] hover:text-[#fff] ease-in duration-300 text-lg font-medium mb-2 p-[15px]">Home</NavLink>
-        <NavLink exact to="/Specialization" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] hover:text-[#fff] ease-in duration-300 text-lg font-medium mb-2 p-[15px]">My Reservations</NavLink>
-        <NavLink exact to="/SpecialCases" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] hover:text-[#fff] ease-in duration-300 text-lg font-medium mb-2 p-[15px]">Book Appointment</NavLink>
+        <NavLink exact to="/Specialization" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] hover:text-[#fff] ease-in duration-300 text-lg font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
+        <NavLink exact to="/SpecialCases" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] hover:text-[#fff] ease-in duration-300 text-lg font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
       </div>
     </div>
   );

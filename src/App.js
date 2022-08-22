3d011551bc84ff86479c5dd1bdedcce8dbdba7ac
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-22 17:04:32 +0200

    Add sign page to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Specialization from './components/BookAppointment/BookAppointment';
 import SpecialCases from './components/MyReservations/MyReservations';
-import LoginPage from './components/LoginPage/LoginPage';
+import LoginPage from './components/Authentication/LoginPage/LoginPage';
 
 function App() {
   return (
     <>
-      <LoginPage />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/Specialization" exact element={<Specialization />} />

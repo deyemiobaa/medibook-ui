a1f94f2e4725471ab932f441d819b87d10e93d65
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-22 17:15:12 +0200

    Update sign up page

--- src/components/Authentication/SignUpPage/SignUpPage.js
+++ src/components/Authentication/SignUpPage/SignUpPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function SignUpPage() {
   return (
@@ -12,6 +12,11 @@ export default function SignUpPage() {
               </p>
               <div className="mb-2">
                 <div className=" relative ">
+                  <input type="text" id="login-with-bg-full-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="full name" />
+                </div>
+              </div>
+              <div className="mb-2">
+                <div className=" relative ">
                   <input type="text" id="login-with-bg-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="email" />
                 </div>
               </div>
@@ -20,14 +25,19 @@ export default function SignUpPage() {
                   <input type="text" id="login-with-bg-password" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="password" />
                 </div>
               </div>
+              <div className="mb-2">
+                <div className=" relative ">
+                  <input type="text" id="login-with-bg-password" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="confirm password" />
+                </div>
+              </div>
               <div className="flex items-center justify-between mt-4">
                 <button type="submit" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                  Validate
+                  Sign Up
                 </button>
               </div>
               <div className="text-center">
                 <a href="vid.com" className="right-0 inline-block text-sm font-light align-baseline text-500 hover:text-gray-800">
-                  Create an account
+                  Login
                 </a>
               </div>
             </form>
@@ -35,5 +45,5 @@ export default function SignUpPage() {
         </div>
       </div>
     </div>
-  )
+  );
 }

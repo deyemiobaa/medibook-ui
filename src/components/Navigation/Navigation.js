a841d92dd3ad491ef4880b9c2d96c115369263f5
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-24 17:59:12 +0200

    Update navbar buttons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assests/logo.png';
-import menu from '../../assests/'
 
 export default function Navigation() {
+  let [open, setOpen] = useState(false)
   return (
     <div className="navigation w-[100%]  sm:w-[25%] bg-gray-300 flex flex-col sm:max-h-full ">
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
         <img src={logo} className="logo rounded-full" alt="logo" />
       </div>
-      <div></div>
-      <div className="navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] w-[100%] sm:pl-[15px] pb-12 pt-0">
+      <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
+        <ion-icon name={open ? 'close-outline' : 'menu-outline'}></ion-icon>
+      </div>
+
+      <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] w-[100%] sm:pl-[15px] pb-12 pt-0` }>
         <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
         <NavLink exact to="/Specialization" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
         <NavLink exact to="/SpecialCases" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>

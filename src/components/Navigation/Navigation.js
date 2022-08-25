628f474ffdb85ce5053bf45d28f6cf89416a4c47
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-25 09:22:31 +0200

    Update nav responsive

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../assests/logo.png';
 
 export default function Navigation() {
-  let [open, setOpen] = useState(false)
+  let [open, setOpen] = useState(false);
+
   return (
     <div className="navigation w-[100%]  sm:w-[25%] bg-gray-300 flex flex-col sm:max-h-full ">
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
@@ -13,7 +14,7 @@ export default function Navigation() {
         <ion-icon name={open ? 'close-outline' : 'menu-outline'}></ion-icon>
       </div>
 
-      <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] w-[100%] sm:pl-[15px] pb-12 pt-0` }>
+      <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] w-[100%] sm:pl-[15px] pb-12 pt-0 ${open ? 'top-20 opacity-100 bg-slate-600' : 'top-[-490px]'}`}>
         <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
         <NavLink exact to="/Specialization" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
         <NavLink exact to="/SpecialCases" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>

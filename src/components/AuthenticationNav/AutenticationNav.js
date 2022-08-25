9413452d124e72484ef6c017217a504b9d4e44b3
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-25 15:52:40 +0200

    Update Login page

--- src/components/AuthenticationNav/AutenticationNav.js
+++ src/components/AuthenticationNav/AutenticationNav.js
@@ -4,16 +4,15 @@ import logo from '../../assests/logo.png';
 
 export default function AutenticationNav() {
   return (
-    <div className="navigation w-[100%] fixed sm:static sm:w-[25%] bg-gray-300 flex flex-col sm:max-h-full ">
+    <div className="navigation w-[100%] sm:w-[25%] bg-gray-300 flex flex-col sm:max-h-full ">
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
         <img src={logo} className="logo rounded-full" alt="logo" />
       </div>
 
-      <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] fixed w-[100%] sm:pl-[15px] pb-12 pt-0 ${open ? 'top-20 bg-gray-300' : ' hidden sm:block'}`}>
-        <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
-        <NavLink exact to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
-        <NavLink exact to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
+      <div className="navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] fixed w-[100%] sm:pl-[15px] pb-12 pt-0">
+        <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Login</NavLink>
+        <NavLink exact to="/Signup" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Sign up</NavLink>
       </div>
     </div>
-  )
+  );
 }

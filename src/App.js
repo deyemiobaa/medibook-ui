1af2856a4884f7113e3165a0a04e5e8e1b42bff0
IfzaRasool <ifzarasoolarain@gmail.com>
2022-08-25 11:39:49 +0300

    fix error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Specialization from './components/BookAppointment/BookAppointment';
-import SpecialCases from './components/MyReservations/MyReservations';
+import BookAppointment from './components/BookAppointment/BookAppointment';
+import MyReservations from './components/MyReservations/MyReservations';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/Specialization" exact element={<Specialization />} />
-        <Route path="/SpecialCases" exact element={<SpecialCases />} />
+        <Route path="/my-reservations" exact element={<MyReservations />} />
+        <Route path="/book-appointment" exact element={<BookAppointment />} />
       </Routes>
     </>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assests/logo.png';
@@ -10,14 +12,14 @@ export default function Navigation() {
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
         <img src={logo} className="logo rounded-full" alt="logo" />
       </div>
-      <div onClick={()=> setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
+      <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
         <ion-icon name={open ? 'close-outline' : 'menu-outline'} />
       </div>
 
       <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] w-[100%] sm:pl-[15px] pb-12 pt-0 ${open ? 'top-20 opacity-100 bg-slate-600' : 'top-[-490px]'}`}>
         <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
-        <NavLink exact to="/Specialization" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
-        <NavLink exact to="/SpecialCases" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
+        <NavLink exact to="/my-reservation" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
+        <NavLink exact to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
       </div>
     </div>
   );

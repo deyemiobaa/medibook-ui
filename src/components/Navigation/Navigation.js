5a99b7d3e3b1e75f16824c908c1e218a382bad38
IfzaRasool <ifzarasoolarain@gmail.com>
2022-08-25 19:49:39 +0300

    Update Mobile menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assests/logo.png';
@@ -12,14 +10,22 @@ export default function Navigation() {
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
         <img src={logo} className="logo rounded-full" alt="logo" />
       </div>
-      <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
+      <button type="button" onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
         <ion-icon name={open ? 'close-outline' : 'menu-outline'} />
+      </button>
+
+      <div className={`open ${open ? 'visible' : 'hidden'}`}>
+        <div className="navLinks flex flex-col mt-44 pl-[15px]">
+          <NavLink to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
+          <NavLink to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
+          <NavLink to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
+        </div>
       </div>
 
-      <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] fixed w-[100%] sm:pl-[15px] pb-12 pt-0 ${open ? 'top-20 bg-gray-300' : ' hidden '}`}>
-        <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
-        <NavLink exact to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
-        <NavLink exact to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
+      <div className="navLinks hidden sm:flex sm:flex-col mt-44 pl-[15px]">
+        <NavLink to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
+        <NavLink to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
+        <NavLink to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
       </div>
     </div>
   );

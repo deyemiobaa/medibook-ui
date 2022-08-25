7ee49af1df9979c87b80d0986a07533c0514e822
IfzaRasool <ifzarasoolarain@gmail.com>
2022-08-25 11:40:29 +0300

    fix error

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -18,7 +18,7 @@ export default function Navigation() {
 
       <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] w-[100%] sm:pl-[15px] pb-12 pt-0 ${open ? 'top-20 opacity-100 bg-slate-600' : 'top-[-490px]'}`}>
         <NavLink exact to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
-        <NavLink exact to="/my-reservation" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
+        <NavLink exact to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
         <NavLink exact to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
       </div>
     </div>

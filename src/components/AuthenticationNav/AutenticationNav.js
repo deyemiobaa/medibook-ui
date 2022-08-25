ace741bb8da2056ac7babcf8d287f7c5eadd4540
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-25 15:36:49 +0200

    Add authentication nav

--- src/components/AuthenticationNav/AutenticationNav.js
+++ src/components/AuthenticationNav/AutenticationNav.js
@@ -1,19 +1,12 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assests/logo.png';
 
-export default function Navigation() {
-  const [open, setOpen] = useState(false);
-
+export default function AutenticationNav() {
   return (
     <div className="navigation w-[100%] fixed sm:static sm:w-[25%] bg-gray-300 flex flex-col sm:max-h-full ">
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
         <img src={logo} className="logo rounded-full" alt="logo" />
-      </div>
-      <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
-        <ion-icon name={open ? 'close-outline' : 'menu-outline'} />
       </div>
 
       <div className={` navLinks flex flex-col  mt-44 items-center sm:items-start h-[10%] fixed w-[100%] sm:pl-[15px] pb-12 pt-0 ${open ? 'top-20 bg-gray-300' : ' hidden sm:block'}`}>
@@ -22,5 +15,5 @@ export default function Navigation() {
         <NavLink exact to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
       </div>
     </div>
-  );
+  )
 }

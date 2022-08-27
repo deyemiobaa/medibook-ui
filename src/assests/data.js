2d886ba23702336363734e3915fed25c0850f684
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-27 10:34:17 +0100

    add props to home

---
+++ src/assests/data.js
@@ -0,0 +1,22 @@
+const doctorData = [
+  {
+    id: 1,
+    name: 'Rabia Naeem',
+    email: 'rabia.naeem@gmail.com',
+    speciality: 'Dentist',
+  },
+  {
+    id: 1,
+    name: 'Rabia Naeem',
+    email: 'rabia.naeem@gmail.com',
+    speciality: 'Dentist',
+  },
+  {
+    id: 1,
+    name: 'Rabia Naeem',
+    email: 'rabia.naeem@gmail.com',
+    speciality: 'Dentist',
+  },
+];
+
+export default doctorData;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,104 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import doctorData from '../../assests/data';
 import doctorImg from '../../assests/doctor1.jpg';
-import Navigation from '../Navigation/Navigation';
 
 const Home = () => (
-  <div className="sm:flex">
-    <Navigation />
-    <section className="text-gray-600 w-[80%] body-font overflow-scroll ml-50">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-20">
-          <h1 className="text-2xl title-font mb-4 text-gray-900 font-black">
-            Welcome to Dashboard
-          </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Directory of all preferred Doctors
-          </p>
-        </div>
-        <div className="flex flex-wrap -m-4">
-          <div className="p-4 lg:w-1/3 md:w-1/2">
-            <div className="h-full flex flex-col items-center text-center">
-              <img
-                alt="team"
-                className="rounded-full object-cover object-center mb-4"
-                src={doctorImg}
-              />
-              <div className="w-full">
-                <h2 className="title-font font-bold text-lg text-gray-900">
-                  Rabia Naeem
-                </h2>
-                <h3 className="text-gray-500 mb-3">Heart Specialist</h3>
-                <p className="mb-4">rabia.naeem@gmail.com</p>
-                <span className="inline-flex">
-                  <Link exact to="../doctor-details">
-                    <button
-                      type="button"
-                      className="bg-lime-400 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full"
-                    >
-                      View Doctor
-                    </button>
-                  </Link>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="p-4 lg:w-1/3 md:w-1/2">
-            <div className="h-full flex flex-col items-center text-center">
-              <img
-                alt="team"
-                className="rounded-full object-cover object-center mb-4"
-                src={doctorImg}
-              />
-              <div className="w-full">
-                <h2 className="title-font font-bold text-lg text-gray-900">
-                  Rabia Naeem
-                </h2>
-                <h3 className="text-gray-500 mb-3">Heart Specialist</h3>
-                <p className="mb-4">rabia.naeem@gmail.com</p>
-                <span className="inline-flex">
-                  <Link exact to="../DoctorDetails">
-                    <button
-                      type="button"
-                      className="bg-lime-400 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full"
-                    >
-                      View Doctor
-                    </button>
-                  </Link>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="p-4 lg:w-1/3 md:w-1/2">
-            <div className="h-full flex flex-col items-center text-center">
-              <img
-                alt="team"
-                className="rounded-full object-cover object-center mb-4"
-                src={doctorImg}
-              />
-              <div className="w-full">
-                <h2 className="title-font font-bold text-lg text-gray-900">
-                  Rabia Naeem
-                </h2>
-                <h3 className="text-gray-500 mb-3">Heart Specialist</h3>
-                <p className="mb-4">rabia.naeem@gmail.com</p>
-                <span className="inline-flex">
-                  <Link exact to="../DoctorDetails">
-                    <button
-                      type="button"
-                      className="bg-lime-400 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full"
-                    >
-                      View Doctor
-                    </button>
-                  </Link>
-                </span>
-              </div>
+  <section className="px-5 py-24 text-gray-600 sm:py-16 sm:w-4/5">
+    <div className="flex flex-col items-center w-full mb-20 text-center">
+      <h1 className="mb-4 text-2xl font-black text-gray-900 title-font">
+        Welcome to Medi Book
+      </h1>
+      <p className="mx-auto text-base leading-relaxed lg:w-2/3">
+        This is the list of all available Doctors
+      </p>
+    </div>
+    <div className="flex flex-wrap m-4">
+      {doctorData.map((doctor) => (
+        <div key={doctor.id} className="p-4 lg:w-1/3 md:w-1/2">
+          <div className="flex flex-col items-center h-full text-center">
+            <img
+              alt="team"
+              className="object-cover object-center mb-4 rounded-full"
+              src={doctorImg}
+            />
+            <div className="w-full">
+              <h2 className="text-lg font-bold text-gray-900 title-font">
+                {doctor.name}
+              </h2>
+              <h3 className="mb-3 text-gray-500">{doctor.speciality}</h3>
+              <p className="mb-4">{doctor.email}</p>
+              <span className="inline-flex">
+                <Link to="/doctor-details">
+                  <button
+                    type="button"
+                    className="px-4 py-2 font-bold text-white rounded-full bg-lime-400 hover:bg-lime-700"
+                  >
+                    View Doctor
+                  </button>
+                </Link>
+              </span>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </div>
+      ))}
+    </div>
+  </section>
 );
 
 export default Home;

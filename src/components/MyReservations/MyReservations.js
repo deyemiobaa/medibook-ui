c55d89837d2313a7fdd083056060a6f0467554c6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-27 10:32:50 +0100

    make nav reusable

--- src/components/MyReservations/MyReservations.js
+++ src/components/MyReservations/MyReservations.js
@@ -1,86 +1,81 @@
 import React from 'react';
-import Navigation from '../Navigation/Navigation';
 import doctorImg from '../../assests/doctor1.jpg';
 import doctorImg2 from '../../assests/doctor2.jpg';
 
 export default function MyReservations() {
   return (
-    <div className="sm:flex">
-      <Navigation />
-      <div className="w-full w-[80%] reservations-container ">
-        <div className="flex flex-col text-center w-full mt-40 sm:mt-0">
-          <h1 className="text-2xl title-font mb-4 text-gray-900 font-black">
-            Reservations
-          </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Find the list of all reserved doctors here
-          </p>
-        </div>
-        <div className="flex m-[2%] sm:m-20 bg-white dark:bg-gray-800 rounded-lg shadow">
-          <div className="flex-none w-24 md:w-48 relative">
-            <img src={doctorImg} alt="doctor" className=" rounded-lg  w-full h-full object-cover" />
-          </div>
-          <div className="flex-auto p-6">
-            <div className="flex flex-wrap">
-              <h1 className="flex-auto text-xl font-semibold dark:text-gray-50">
-                Jannet Jackson
-              </h1>
-              <div className="text-xl font-semibold text-gray-500 dark:text-gray-300">
-                Date: 12.03.2022
-              </div>
-              <div className="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2">
-                Speciality:
-                {' '}
-                <span className="text-xl text-black"> Neurosurgeon</span>
-                {' '}
-                <br />
-                jannetjackson@gmail.com
-              </div>
-            </div>
-            <p className="text-sm text-gray-500 dark:text-gray-300">
-              motif: continuous headaches and nausea
-            </p>
-            <div className="flex mb-4 text-sm font-medium">
-              <button type="button" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                Meet Now
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex  m-[2%] sm:m-20 bg-white dark:bg-gray-800 rounded-lg shadow">
-          <div className="flex-none w-24 md:w-48  relative">
-            <img src={doctorImg2} alt="doctor" className="absolute rounded-lg inset-0 w-full h-full object-cover" />
-          </div>
-          <div className="flex-auto p-6">
-            <div className="flex flex-wrap">
-              <h1 className="flex-auto text-xl font-semibold dark:text-gray-50">
-                Auriane Kibala
-              </h1>
-              <div className="text-xl font-semibold text-gray-500 dark:text-gray-300">
-                Date: 12.03.2022
-              </div>
-              <div className="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2">
-                Speciality:
-                {' '}
-                <span className="text-xl text-black">Therapist</span>
-                {' '}
-                <br />
-                aurianekibala@gmail.com
-              </div>
-            </div>
-            <p className="text-sm text-gray-500 dark:text-gray-300">
-              motif: depression and anxiety
-            </p>
-            <div className="flex mb-4 text-sm font-medium">
-              <button type="button" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                Meet Now
-              </button>
-            </div>
-          </div>
-        </div>
-
+    <section className="px-5 py-24 text-gray-600 sm:py-16 sm:w-4/5">
+      <div className="flex flex-col w-full mb-10 text-center">
+        <h1 className="mb-4 text-2xl font-black text-gray-900 title-font">
+          Appointments
+        </h1>
+        <p className="mx-auto text-base leading-relaxed lg:w-2/3">
+          Here is the list of all booked doctors.
+        </p>
       </div>
-    </div>
+      <div className="flex m-[2%] sm:m-20 bg-white dark:bg-gray-800 rounded-lg shadow">
+        <div className="relative flex-none w-24 md:w-48">
+          <img src={doctorImg} alt="doctor" className="object-cover w-full h-full rounded-lg " />
+        </div>
+        <div className="flex-auto p-6">
+          <div className="flex flex-wrap">
+            <h1 className="flex-auto text-xl font-semibold dark:text-gray-50">
+              Jannet Jackson
+            </h1>
+            <div className="text-xl font-semibold text-gray-500 dark:text-gray-300">
+              Date: 12.03.2022
+            </div>
+            <div className="flex-none w-full mt-2 text-sm font-medium text-gray-500 dark:text-gray-300">
+              Speciality:
+              {' '}
+              <span className="text-xl text-black"> Neurosurgeon</span>
+              {' '}
+              <br />
+              jannetjackson@gmail.com
+            </div>
+          </div>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+            motif: continuous headaches and nausea
+          </p>
+          <div className="flex mb-4 text-sm font-medium">
+            <button type="button" className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-indigo-600 rounded-lg shadow-md hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 ">
+              Meet Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex  m-[2%] sm:m-20 bg-white dark:bg-gray-800 rounded-lg shadow">
+        <div className="relative flex-none w-24 md:w-48">
+          <img src={doctorImg2} alt="doctor" className="absolute inset-0 object-cover w-full h-full rounded-lg" />
+        </div>
+        <div className="flex-auto p-6">
+          <div className="flex flex-wrap">
+            <h1 className="flex-auto text-xl font-semibold dark:text-gray-50">
+              Auriane Kibala
+            </h1>
+            <div className="text-xl font-semibold text-gray-500 dark:text-gray-300">
+              Date: 12.03.2022
+            </div>
+            <div className="flex-none w-full mt-2 text-sm font-medium text-gray-500 dark:text-gray-300">
+              Speciality:
+              {' '}
+              <span className="text-xl text-black">Therapist</span>
+              {' '}
+              <br />
+              aurianekibala@gmail.com
+            </div>
+          </div>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+            motif: depression and anxiety
+          </p>
+          <div className="flex mb-4 text-sm font-medium">
+            <button type="button" className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-indigo-600 rounded-lg shadow-md hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 ">
+              Meet Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,30 +1,38 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import logo from '../../assests/logo.png';
 
-export default function Navigation() {
+export default function Navigation({ urls }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="navigation mb-30 sm:w-[20%]  bg-gray-300 flex flex-col sm:max-h-full ">
+    <div className="navigation sm:w-[20%] bg-white flex justify-between items-center sm:flex-col sm:border-r sm:border-r-gray-400 border-b border-b-gray-400 fixed top-0 left-0 sm:bottom-0 px-3 sm:justify-start">
       <div className="logo w-[20%] sm:w-[70%] p-[10px] sm:self-center">
-        <img src={logo} className="logo rounded-full" alt="logo" />
+        <img src={logo} className="rounded-full logo" alt="logo" />
       </div>
-      <button type="button" onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer sm:hidden">
+      <button type="button" onClick={() => setOpen(!open)} className="text-3xl cursor-pointer sm:hidden">
         <ion-icon name={open ? 'close-outline' : 'menu-outline'} />
       </button>
-      <div className={`open ${open ? 'visible' : 'hidden'}`}>
-        <div className="navLinks flex flex-col mt-44 pl-[15px]">
-          <NavLink to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
-          <NavLink to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
-          <NavLink to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
+      <div className={`open ${open ? 'fixed mt-56 z-20 left-0 right-0' : 'hidden'}`}>
+        <div className="flex flex-col pl-3 bg-white navLinks first:border-t last:border-b">
+          {urls.map(({ path, name }) => (
+            <NavLink to={path} className="py-2 text-xl ease-in duration-300 sm:text-[90%] font-medium mb-2" key={name} style={{ background: 'white', color: 'black' }}>{name}</NavLink>
+          ))}
         </div>
       </div>
       <div className="navLinks hidden sm:flex sm:flex-col mt-44 pl-[15px]">
-        <NavLink to="/" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]">Home</NavLink>
-        <NavLink to="/my-reservations" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">My Reservations</NavLink>
-        <NavLink to="/book-appointment" activeClassName="nav-active" className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px] whitespace-nowrap">Book Appointment</NavLink>
+        {urls.map(({ path, name }) => (
+          <NavLink to={path} className="link-el hover:bg-[#97C02C] text-xl hover:text-[#fff] ease-in duration-300 sm:text-[90%] font-medium mb-2 p-[15px]" key={name}>{name}</NavLink>
+        ))}
       </div>
     </div>
   );
 }
+
+Navigation.propTypes = {
+  urls: PropTypes.arrayOf(PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+};

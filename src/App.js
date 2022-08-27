1bf672f1d9b8d21a9f1d7b7b0e21999c97a47926
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-27 10:33:47 +0100

    add nav as default in app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import BookAppointment from './components/BookAppointment/BookAppointment';
 import MyReservations from './components/MyReservations/MyReservations';
+import LoginPage from './components/Authentication/LoginPage/LoginPage';
+import SignUpPage from './components/Authentication/SignUpPage/SignUpPage';
+import Navigation from './components/Navigation/Navigation';
+import { authenticatedNav } from './assests/navigation';
 
 function App() {
   return (
-    <>
+    <div className="grid min-h-screen justify-items-end">
+      <Navigation urls={authenticatedNav} />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/my-reservations" exact element={<MyReservations />} />
-        <Route path="/book-appointment" exact element={<BookAppointment />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/appointments" element={<MyReservations />} />
+        <Route path="/book-appointment" element={<BookAppointment />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 

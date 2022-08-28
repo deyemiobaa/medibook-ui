86335cb2099947ec640d74f75d867c6af06cf5c6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-28 21:47:52 +0100

    add reusable hook for inputs

---
+++ src/components/Authentication/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const useForm = (initalValues) => {
+  const [values, setValues] = useState(initalValues);
+
+  const handleChange = (event) => {
+    setValues((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return {
+    values,
+    handleChange,
+  };
+};
+
+export default useForm;

9004649d13828bd6a0ca303ffc68d956a6c5aaeb
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-28 17:45:22 +0100

    split routes to render in app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home';
 import BookAppointment from './components/BookAppointment/BookAppointment';
 import MyReservations from './components/MyReservations/MyReservations';
 import LoginPage from './components/Authentication/LoginPage/LoginPage';
 import SignUpPage from './components/Authentication/SignUpPage/SignUpPage';
 import Navigation from './components/Navigation/Navigation';
-import { authenticatedNav } from './assests/navigation';
+import { authenticatedNav, unauthenticatedNav } from './assests/navigation';
+import storage from './app/localStorage';
 
 function App() {
+  if (storage.get('token')) {
+    return (
+      <div className="grid min-h-screen justify-items-end">
+        <Navigation urls={authenticatedNav} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointments" element={<MyReservations />} />
+          <Route path="/book-appointment" element={<BookAppointment />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    );
+  }
   return (
     <div className="grid min-h-screen justify-items-end">
-      <Navigation urls={authenticatedNav} />
+      <Navigation urls={unauthenticatedNav} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/appointments" element={<MyReservations />} />
-        <Route path="/book-appointment" element={<BookAppointment />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/register" element={<SignUpPage />} />
       </Routes>
     </div>
   );

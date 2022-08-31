e4c444fa7636c052dee6bba559f66b68742a74c9
sainingo <nickmentor421@gmail.com>
2022-08-31 12:57:28 +0300

    Create a doctor slice

--- src/redux/doctors/doctorsSlice.js
+++ src/redux/doctors/doctorsSlice.js
@@ -1,11 +1,24 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import getDoctors from './doctorsApi';
+import getDoctors, { postDoctor, deleteDoctor } from './doctorsApi';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 
 export const fetchDoctors = createAsyncThunk('doctors/fetchDoctors', async () => {
   const doctors = await getDoctors(baseURL);
   return doctors;
+});
+
+export const postDoctors = createAsyncThunk('doctors/post', async (args) => {
+  const {
+    name, specialization, hourly, available, picture,
+  } = args;
+  const response = await postDoctor(baseURL, name, specialization, hourly, available, picture);
+  return response;
+});
+
+export const deleteDoctors = createAsyncThunk('doctors/delete', async (id) => {
+  const res = await deleteDoctor(baseURL, id);
+  return res;
 });
 
 const initialState = {
@@ -30,6 +43,13 @@ const doctorsSlice = createSlice({
       state.loading = false;
       state.doctors = [];
       state.error = action.error.message;
+    },
+    [postDoctors.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.doctors = action.payload;
+    },
+    [deleteDoctor.fulfilled]: (state) => {
+      state.loading = false;
     },
   },
 });

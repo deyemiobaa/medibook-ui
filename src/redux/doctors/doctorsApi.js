e4bd5986ed5959970e8696325a3cfe6f8f554c2d
sainingo <nickmentor421@gmail.com>
2022-08-31 12:56:59 +0300

    post doctor ti api

--- src/redux/doctors/doctorsApi.js
+++ src/redux/doctors/doctorsApi.js
@@ -15,4 +15,39 @@ const getDoctors = async (baseURL) => {
   return result; // { message: '...', status: '...' }
 };
 
+export const postDoctor = async (baseURL, name, specialization, hourly, available, picture) => {
+  const response = await fetch(`${baseURL}/doctors`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        specialization,
+        hourly_rate: hourly,
+        available_times: available,
+        picture,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+    });
+  const result = await response.json();
+  return result; // { message: '...', status: '...' }
+};
+
+export const deleteDoctor = async (baseURL, id) => {
+  const response = await fetch(`${baseURL}/doctors/${id}`,
+    {
+      method: 'DELETE',
+      body: JSON.stringify({
+        id,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+    });
+  const result = await response.json();
+  return result;
+};
 export default getDoctors;

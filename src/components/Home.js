3a0c78b4b8e4e0ce2056140dcea77e3adfa7d9aa
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-09-01 09:40:28 +0100

    fix style

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,13 +23,11 @@ export default function Home() {
       <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3">
         {doctors.map((doctor) => (
           <div key={doctor.id} className="flex flex-col items-center h-full p-4 text-center">
-            <div className="">
-              <img
-                alt={doctor.name}
-                className="object-cover object-center mb-4 rounded-full"
-                src={doctor.picture}
-              />
-            </div>
+            <img
+              alt={doctor.name}
+              className="w-auto h-auto mb-4 rounded-full"
+              src={doctor.picture}
+            />
             <div className="w-full">
               <h2 className="text-lg font-bold text-gray-900 title-font">
                 {doctor.name}

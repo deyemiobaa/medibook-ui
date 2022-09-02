29cd7d51624d46a8d7e5ce7e6455265b6d437ea6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-09-02 07:13:07 +0100

    add Home component test

---
+++ src/mocks/store.js
@@ -0,0 +1,19 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from '../redux/users/usersSlice';
+import doctorsReducer from '../redux/doctors/doctorsSlice';
+import appointmentsReducer from '../redux/appointments/appointmentsSlice';
+
+// Create the root reducer independently to obtain the RootState type
+
+const rootReducer = combineReducers({
+  users: userReducer,
+  doctors: doctorsReducer,
+  appointments: appointmentsReducer,
+})
+
+export default function setupStore(preloadedState) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState
+  })
+}
